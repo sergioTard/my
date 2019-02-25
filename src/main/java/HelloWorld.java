bc23c0fc1dc1be846c2bade86e6e861a879e252d
Mikhail Nechaev <mikhail.nechaev@corp.mail.ru>
2019-02-25 20:47:01 +0300

    hello world

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+import ru.mail.polis.open.HelloWorld2;
+
+/**
+ * @author mikhail.nechaev
+ * Since 25/02/2019
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        HelloWorld2.main(null);
+    }
+}
---
+++ src/main/java/ru/mail/polis/open/HelloWorld2.java
@@ -0,0 +1,13 @@
+package ru.mail.polis.open;
+
+
+/**
+ * @author mikhail.nechaev
+ * Since 25/02/2019
+ */
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world2!");
+    }
+}
