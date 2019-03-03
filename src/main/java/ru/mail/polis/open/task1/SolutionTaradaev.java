d533367008a8a574772a1ec4ad29b41a5867da8f
Сергей т <sergio.tardv@gmail.com>
2019-03-04 01:51:30 +0300

    TaradaevSol

---
+++ src/main/java/ru/mail/polis/open/task1/SolutionTaradaev.java
@@ -0,0 +1,22 @@
+package ru.mail.polis.open.task1;
+
+public class SolutionTaradaev implements FizzBuzz {
+  @Override
+  public void print(int from, int to) {
+    String FIZZ = "FIZZ";
+    String BUZZ = "BUZZ";
+
+
+    for (int i = from; i < to + 1; i++) {
+
+      String Results = "";
+      if (i % 3 == 0 && i != 0) Results = BUZZ;
+      if (i % 5 == 0 && i != 0) Results += FIZZ;
+      if (Results.equals("")) {
+        System.out.println(i);
+      } else System.out.println(Results);
+
+    }
+
+  }
+}
